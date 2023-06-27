264eb4b410a99f7276e8afb7103ec3dbc4e096eb
Debbroto Kumar Karmokar <136155528+debbrotokumark@users.noreply.github.com>
2023-06-28 01:39:41 +0600

    Create printsmalllattertobig.c

---
+++ printsmalllattertobig.c
@@ -0,0 +1,31 @@
+//print small latter to big 
+
+
+#include <stdio.h>
+int main()
+{
+int a[]={2,3,4,5,3,5,6,7};
+//gets(a);
+int i,j,b,c;
+b=sizeof(a)/sizeof(0);
+
+for(i=0;i<b-1;i++)
+{
+    for(j=0;j<=b-1;j++)
+    {
+        if(a[j]>a[j+1])
+        {
+            int t=a[j];;
+            a[j]=a[j+1];
+            a[j+1]=t;
+
+        }
+    }
+
+}
+for(i=0;i<b;i++)
+{
+printf("%d  ",a[i]);
+}
+
+}
