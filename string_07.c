697a0a0fe370ab22246b342b315739c42f0071ee
Debbroto Kumar Karmokar <136155528+debbrotokumark@users.noreply.github.com>
2023-06-28 08:49:54 +0600

    Create string_07.c

---
+++ string_07.c
@@ -0,0 +1,21 @@
+//Write a program to count the number of characters, words and lines in the given text
+#include<stdio.h>
+
+int main(){
+
+char a[1000];
+gets(a);
+
+long long int c;
+for(c=0;a[c]!='\0';c++)
+{
+
+}
+ printf("%lld",c);
+
+
+// printf("%lld",strlen(a));
+return 0;
+}
+
+
