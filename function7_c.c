5e22145987fba559c3f063654939814cb20a597f
Debbroto Kumar Karmokar <136155528+debbrotokumark@users.noreply.github.com>
2023-06-29 14:17:00 +0600

    Create function7_c.c

---
+++ function7_c.c
@@ -0,0 +1,17 @@
+//c) without argument without return type
+
+#include <stdio.h>
+
+void addn()
+{
+     double  a,b ;
+    scanf("%lf %lf",&a,&b);
+printf("Add Two Number :  %.3lf",(a+b));
+
+}
+void main() {
+
+printf("Enter Your Two Number : ");
+addn();
+
+}
