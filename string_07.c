a7b11111bbf257acddf2a86a76370a4cdc0c88fa
Debbroto Kumar Karmokar <136155528+debbrotokumark@users.noreply.github.com>
2023-07-03 22:33:45 +0600

    Update string_07.c

--- string_07.c
+++ string_07.c
@@ -1,21 +1,22 @@
 //Write a program to count the number of characters, words and lines in the given text
 #include<stdio.h>
-
 int main(){
-
-char a[1000];
-gets(a);
-
-long long int c;
-for(c=0;a[c]!='\0';c++)
-{
-
+char text[] = "Hello, world!\nHow are you?\nI hope you're doing well.";
+long long int c,d=1,e=1;
+for(c=0;text[c]!='\0';c++){}
+ printf("Total char :  %lld\n",c);
+for(c=0;text[c]!='\0';c++){
+if(text[c]==' ' || text[c]=='\t' || text[c]=='\n')
+ d++;
 }
- printf("%lld",c);
-
-
-// printf("%lld",strlen(a));
-return 0;
+ printf("Total word :  %lld\n",d);
+ for(c=0;text[c]!='\0';c++){
+ if(text[c]=='\n')
+        e++;
+ }
+  printf("Total line :  %lld\n",e);
 }
+
+
 
 
